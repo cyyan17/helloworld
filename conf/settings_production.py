728b4e3aab895208105e10af7dc8fdef3fe7a547
cyyan17 <cheny.17@163.com>
2018-08-24 14:14:45 +0800

    nite commit

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -22,7 +22,7 @@ DATABASES = {
         'NAME': 'helloworld',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': '',                        # 你的数据库password
-        'HOST': '192.168.26.131',                   		   # 数据库HOST
+        'HOST': '192.168.26.131',                # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
